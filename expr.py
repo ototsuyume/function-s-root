fbd03730ac943a407dd51068ead2020fb7cd4cd1
ototsuyume <ototsuyume@gmail.com>
2012-04-29 17:58:29 +0800

    commit

---
+++ expr.py
@@ -0,0 +1,238 @@
+#! /usr/bin/python
+import sys
+import math
+
+class expr(object):
+    _left = 0
+    _right = 0
+    _content = 0
+    _phr=0
+    def __init__(self,l,r):
+        self._left = l
+        self._right = r
+
+    def differention(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def set_priority(self):
+        self._phr=1
+
+    def get_node(self):
+        if self._phr==1: 
+            return '('+self._left.get_node()+self._content+self._right.get_node()+')'
+        else:
+            return self._left.get_node()+self._content+self._right.get_node()
+
+    def _simplify(self,l,r):
+        pass
+
+    def simplify(self):
+        r = self._right.simplify()
+        l = self._left.simplify()
+        if isinstance(r,value_expr) and isinstance(l,value_expr):
+            return value_expr(self.evaluate())
+        else:
+            return self._simplify(l,r)
+
+    def containx(self):
+        return self._left.containx() or self._right.containx()
+
+    def __eq__(self,a):
+        if self._content == a._content:
+            return self._left == a._left and self._right == a._right
+        else:
+            return False
+
+class add_expr(expr):
+    _content = '+' 
+    def differention(self):
+        return add_expr(self._left.differention(),self._right.differention())
+    
+    def evaluate(self):
+        return self._left.evaluate()+self._right.evaluate()
+
+    def _simplify(self,l,r):
+        if isinstance(l,value_expr) and l._content==0:
+            return r
+        elif isinstance(r,value_expr) and r._content==0:
+            return l
+        else:
+            self._right=r
+            self._left=l
+            return self
+
+class sub_expr(expr):
+    _content = '-'
+    def differention(self):
+        return sub_expr(self._left.differention(),self._right.differention())
+
+    def evaluate(self):
+        return self._left.evaluate()-self._right.evaluate()
+
+    def _simplify(self,l,r):
+        if  isinstance(l,value_expr) and l._content==0:
+            return mul_expr(value_expr(-1),r)
+        elif  isinstance(r,value_expr) and r._content==0:
+            return l
+        else:
+            self._right=r
+            self._left=l
+            return self
+    
+
+class mul_expr(expr):
+    _content = '*'
+    def differention(self):
+        return add_expr(mul_expr(self._left.differention(),self._right),mul_expr(self._left,self._right.differention()))
+    
+    def evaluate(self):
+        return self._left.evaluate()*self._right.evaluate()
+
+    def _simplify(self,l,r):
+        if  ( isinstance(l,value_expr) and l._content==0) or ( isinstance(r,value_expr) and r._content==0):
+            return value_expr(0)
+        elif isinstance(l,value_expr) and l._content==1:
+            return r
+        elif isinstance(r,value_expr) and  r._content==1:
+            return l
+        else:
+            self._right=r
+            self._left=l
+            return self
+
+ 
+
+
+class div_expr(expr):
+    _content = '/'
+    def differention(self):
+        return div_expr(add_expr(mul_expr(self._left.differention(),self._right),mul_expr(self._left,self._right.differention())),self._right)
+
+    def evaluate(self):
+        return self._left.evaluate()/self._right.evaluate()
+
+    def _simplify(self,l,r):
+        if  l._content==0:
+            return value_expr(0)
+        elif  r._content==0:
+            raise Exception("Error:the value can't divided by 0")
+        elif r._content==1:
+            return l
+        else:
+            self._right=r
+            self._left=l
+            return self
+
+class pow_expr(expr):
+    _content = '^'
+    def differention(self):
+        l = self._left.containx()
+        r = self._right.containx()
+        if l:
+            if r:
+                return pow_expr(exp_value(),mul_expr(self._right,ln_expr(self._left))).differention()
+            else:
+                print("fuck")
+                return mul_expr(self._left.differention(),mul_expr(self._right,pow_expr(self._left,value_expr(self._right.evaluate()-1))))
+        else:
+            if r:
+                return mul_expr(mul_expr(ln_expr(self._left),self),self._right.differention())
+            else:
+                return value_expr(0)
+
+    def evaluate(self):
+        return math.pow(self._left.evaluate(),self._right.evaluate())
+
+    def _simplify(self,l,r):
+        if (isinstance(r,value_expr) and r._content==0) or ( isinstance(l,value_expr) and  l._content==1):
+            return value_expr(1)
+        elif isinstance(l,value_expr) and l._content==0:
+            return value_expr(0)
+        elif isinstance(r,value_expr) and  r._content==1:
+            return l
+        else: 
+            self._left=l
+            self._right=r
+            return self
+    def get_node(self):
+        return '('+self._left.get_node()+'^'+'('+self._right.get_node()+'))'
+
+class ln_expr(expr):
+    def __init__(self,e):
+        self._content=e
+        
+    
+    def evaluate(self):
+        return value_expr(math.log(self._content.evaluate()))
+
+    def differention(self):
+        return div_expr(self._content.differention(),self._content)
+
+    def simplify(self):
+        a=self._content.simplify()
+        if isinstance(a,value_expr):
+            return value_expr(math.log(a._content))
+        else:
+            self._content=a
+            return self
+    
+    def get_node(self):
+        return "ln("+self._content.get_node()+")"
+
+
+
+
+def exp_value():
+    return value_expr(math.e)
+        
+
+class value_expr(expr):
+    def __init__(self,val):
+        self._content = val
+
+    def differention(self):
+        return value_expr(0)
+
+    def evaluate(self):
+        return self._content
+
+    def get_node(self):
+        return str(self._content)
+
+    def simplify(self):
+        return self
+
+    def containx(self):
+        return False
+
+    def __eq__(self,a):
+        return self._content==a._content
+
+class x_expr(expr):
+    _content = 'x'
+    _value = 0
+    def __init__(self):
+        pass
+    def differention(self):
+        return value_expr(1)
+
+    def evaluate(self):
+        return self._value
+
+    def get_node(self):
+        return self._content
+
+    def simplify(self):
+        return self
+
+    def set_value(self,v):
+        self._value=v
+
+    def containx(self):
+        return True
+
+    def __eq__(self,a):
+        return self._content == a._content
---
+++ parse.py
@@ -0,0 +1,158 @@
+#! /usr/bin/python
+
+from expr import *
+from random import randint
+
+TK_NONE = 0
+TK_NUM  = 1
+TK_NAME = 2
+TK_OPR  = 3
+
+x = x_expr()
+
+
+opr={'+':1,'-':1,'*':2,'/':2,'^':3,'(':0,')':0}
+oprexpr={'+':add_expr,'-':sub_expr,'*':mul_expr,'/':div_expr,'^':pow_expr}
+
+nameexpr={'ln':ln_expr,'e':exp_value}
+
+def tokenize(l):
+    i=0
+    j=0
+    last = TK_NONE
+    toklist=[]
+    typelist=[]
+    while j<len(l):
+        if l[j]>='0' and l[j]<='9':
+            if last==TK_NONE:
+                last = TK_NUM
+            elif last!=TK_NUM:
+                typelist.append(last)
+                toklist.append(l[i:j])
+                last = TK_NUM 
+                i=j
+        elif l[j]>='a' and l[j]<='z':
+            if last==TK_NONE:
+                last = TK_NAME
+            elif last!=TK_NAME:
+                typelist.append(last)
+                toklist.append(l[i:j])
+                i=j
+                last=TK_NAME
+        elif l[j] in opr:
+               typelist.append(last)
+               toklist.append(l[i:j])
+               last = TK_OPR
+               i=j
+        j=j+1
+    typelist.append(last)
+    toklist.append(l[i:j])         
+    return (toklist,typelist)
+
+def _parser(tokl,typel):
+    i=0
+    ret=0
+    lastopr='+'
+    nsp=0
+    osp=0
+    numstack=list(range(100))
+    oprstack=list(range(100))
+    print(tokl)
+    while i<len(tokl):
+        print("current token",tokl[i])
+        if typel[i]==TK_NUM:
+            numstack[nsp]=value_expr(int(tokl[i]))
+            ret = numstack[nsp]
+            nsp+=1
+        elif typel[i]==TK_NAME:
+            if tokl[i]=='x':
+                numstack[nsp] = x
+                ret = x
+                nsp+=1
+            elif tokl[i]=='e':
+                numstack[nsp] = exp_value()
+                ret = numstack[nsp]
+                nsp+=1
+            elif tokl[i+1]!='(':
+                raise Exception("Syntax error:the element after name must be a (")
+            #numstack[i]=
+        elif typel[i]==TK_OPR:
+            print("current opr",tokl[i])
+            if tokl[i]=='(':
+                i+=1
+                numstack[nsp],b=_parser(tokl[i:],typel[i:])
+                numstack[nsp].set_priority()
+                i+=b
+                nsp+=1
+            elif tokl[i]==')':
+                break
+            
+            elif osp==0:
+                if nsp==0:
+                    if tokl[i]!='-':
+                        raise Exception("Syntax error:the head of polynomial must be a number or name")
+                    numstack[nsp]=value_expr(-1)
+                    oprstack[osp]='*'
+                    nsp+=1
+                else:
+                    oprstack[osp]=tokl[i]
+                osp+=1
+            elif opr[tokl[i]]>opr[oprstack[osp-1]]:
+                oprstack[osp]=tokl[i]
+                osp+=1
+            else:
+                print("22222222222",tokl[i])
+                lastopr=oprstack[osp-1]
+                while opr[lastopr]>=opr[tokl[i]]:
+                    ret = oprexpr[lastopr](numstack[nsp-2],numstack[nsp-1]) 
+                    numstack[nsp-2]=ret
+                    nsp-=1
+                    osp-=1
+                    if osp==0:
+                        break
+                    lastopr=oprstack[osp-1]
+                oprstack[osp]=tokl[i]
+                osp+=1
+        i+=1
+    print(nsp,osp)
+    while osp>0:
+        ret = oprexpr[oprstack[osp-1]](numstack[nsp-2],numstack[nsp-1])
+        numstack[nsp-2]=ret
+        nsp-=1
+        osp-=1
+    return (ret,i)
+
+
+def find_root(l):
+    tokl,typel = tokenize(l)
+    a,b=_parser(tokl,typel)
+    print("before differention:",a.get_node())
+    c=a.differention()
+    print("Differention:",c.get_node())
+
+    c=c.simplify()
+
+    print("Simplify:",c.get_node())
+    d = sub_expr(x,div_expr(a,c))
+    f1 = randint(0,100)
+    f2 = 0
+    i  = 1
+    while True:
+        x.set_value(f1)
+        f2 = d.evaluate()
+        print("Iterating times:%d value:%f"%(i,f2))
+        if abs(f2-f1) < 0.01*f2 :
+            break
+        x.set_value(f2)
+        if abs(a.evaluate())<=0.01*f2:
+            break
+        f1 = f2
+        i+=1
+    print("The root of the function is:%f"%f2)
+
+
+ 
+                
+if __name__=='__main__':  
+    find_root(sys.argv[1])
+                
